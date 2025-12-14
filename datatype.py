f61afc0fe982d65a98aa35f95c6657b914515b28
24331a05j5-harsita <24331a05j5@mvgrce.edu.in>
2025-12-14 11:39:12 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,16 @@
+
+#illustration of data types
+a=20
+print(a)
+b=2.3
+print(b)
+c='A '
+print(c)
+d="welcome to  our python programming"
+print(d)
+print(a+b)
+print(c+d)
+e='3'
+print(c+e)
+f=" lab"
+print(d+f)
